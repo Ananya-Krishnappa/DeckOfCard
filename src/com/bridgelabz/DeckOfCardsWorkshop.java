71b3f63d2ee7668a790b53a12f7691ee5693037e
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-06-17 19:23:32 +0530

    Added:added player to the game

--- src/com/bridgelabz/DeckOfCardsWorkshop.java
+++ src/com/bridgelabz/DeckOfCardsWorkshop.java
@@ -1,15 +1,30 @@
+/**
+ * Purpose:to initialise deck of cards having suit ("Clubs", "Diamonds", "Hearts", "Spades")
+ *  & Rank ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"). 
+ *  Shuffle the cards using Random method and then distribute 9 Cards to 4 Players 
+ *  and Print the Cards the received by the 4 Players.
+ * @author Ananya K
+ * @version 1.0
+ * @since 17/06/2021
+ * 
+ */
 package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class DeckOfCardsWorkshop {
 	private static String[] suit = { "Spades", "Hearts", "Diamond", "Clubs" };
-	private static String[] rank = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+	private static String[] rank = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
 	private static String[][] deckOfCards = { suit, rank };
-	private static Card[] deck = new Card[52]; 
+	private static Card[] deck = new Card[52];
+	private static List<Player> playerList = new ArrayList<Player>();
 
 	public static void main(String[] args) {
 		DeckOfCardsWorkshop deckOfCard = new DeckOfCardsWorkshop();
 		deckOfCard.gameSetUp();
-		deckOfCard.printDeck();
+		deckOfCard.addPlayer(2);
 	}
 
 	/**
@@ -21,6 +36,31 @@ public class DeckOfCardsWorkshop {
 			for (String value : deckOfCards[1]) {
 				deck[i++] = new Card(s, value);
 			}
+		}
+	}
+
+	/**
+	 * adding players to the game
+	 */
+	private void addPlayer(int numberOfPlayers) {
+		if (numberOfPlayers >= 4) {
+			System.out.println("Maximum of 4 players is allowed");
+		} else {
+			Scanner sc = new Scanner(System.in);
+			for (int i = 0; i < numberOfPlayers; i++) {
+				System.out.println("Enter first name");
+				String fn = sc.nextLine();
+				System.out.println("Enter second name");
+				String ln = sc.nextLine();
+				Player player = new Player();
+				player.setFirstname(fn);
+				player.setLastName(ln);
+				playerList.add(player);
+				for (int j = 0; j < playerList.size(); j++) {
+					System.out.println(playerList.get(j));
+				}
+			}
+			sc.close();
 		}
 	}
 
